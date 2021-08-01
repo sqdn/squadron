999a7aeb12502001698eaadab1ea085ebaf6ac14
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2021-08-01 19:29:32 +0700

    Rename `CommMeta` to `CommPacketMeta`

--- src/communication/comm-packet.ts
+++ src/communication/comm-packet.ts
@@ -1,15 +1,38 @@
 import { TransferListItem } from 'worker_threads';
 
+/**
+ * Communication packet containing command body and meta information.
+ *
+ * Command meta is stored in {@link meta} property. Other properties contain command body.
+ */
 export interface CommPacket {
 
-  readonly meta?: CommMeta | undefined;
+  /**
+   * Command meta, if present.
+   */
+  readonly meta?: CommPacketMeta | undefined;
 
 }
 
-export interface CommMeta {
+/**
+ * Communication command meta information.
+ *
+ * This is an object containing arbitrary fields. Some of them reserved.
+ */
+export interface CommPacketMeta {
 
+  /**
+   * Array of objects to transfer with [postMessage] call.
+   *
+   * [postMessage]: https://nodejs.org/dist/latest-v12.x/docs/api/worker_threads.html#worker_threads_port_postmessage_value_transferlist
+   */
   readonly transferList?: readonly TransferListItem[] | undefined;
 
+  /**
+   * Command sequence stream identifier.
+   *
+   * This value is the same for both request and response.
+   */
   readonly streamId?: string | undefined;
 
   readonly [name: string]: unknown | undefined;
