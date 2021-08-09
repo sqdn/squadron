eb9b983652fa7333607538ec5cc2227ff69c707f
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2021-08-09 16:21:09 +0700

    Rename test

--- src/impl/formation/formation.ctl-channel.spec.ts
+++ src/impl/formation/formation.ctl-channel.spec.ts
@@ -1,7 +1,7 @@
 import { describe, expect, it } from '@jest/globals';
 import { Formation$CtlChannel } from './formation.ctl-channel';
 
-describe('FormationToHubCommChannel', () => {
+describe('Formation$CtlChannel', () => {
   describe('toString', () => {
     it('provides string representation', () => {
       expect(String(Formation$CtlChannel)).toBe('[Formation:CtlChannel]');
