67827ceca648944e60edf39710ca05fa9048c9de
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2021-08-11 16:41:53 +0700

    Disable coverage requirements

--- jest.config.js
+++ jest.config.js
@@ -10,12 +10,7 @@ export default {
   ],
   coverageDirectory: 'target/coverage',
   coverageThreshold: {
-    global: {
-      statements: 100,
-      branches: 100,
-      functions: 100,
-      lines: 100,
-    },
+    global: 0,
   },
   extensionsToTreatAsEsm: ['.ts'],
   moduleNameMapper: {
