6ddd10616ce2be0ac9d6cb835e058289d8c52f2b
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2023-01-27 15:54:52 +0700

    Build with `@run-z/project-config`

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
-export default {
-  preset: 'ts-jest/presets/default-esm',
-  collectCoverage: true,
+import { configureJest } from '@run-z/project-config';
+
+export default await configureJest({
   collectCoverageFrom: [
     'src/**/*.ts',
     '!src/launch/**',
@@ -8,33 +8,4 @@ export default {
     '!src/**/index.ts',
     '!**/node_modules/**',
   ],
-  coverageDirectory: 'target/coverage',
-  coverageThreshold: {
-    global: 100,
-  },
-  extensionsToTreatAsEsm: ['.ts'],
-  reporters: [
-    'default',
-    [
-      'jest-junit',
-      {
-        suiteName: '@sqdn/squadron',
-        outputDirectory: './target/test-results',
-        classNameTemplate: '{classname}: {title}',
-        titleTemplate: '{classname}: {title}',
-        ancestorSeparator: ' › ',
-        usePathForSuiteName: 'true',
-      },
-    ],
-  ],
-  testEnvironment: 'node',
-  transform: {
-    '^.+\\.tsx?$': [
-      'ts-jest',
-      {
-        tsconfig: 'tsconfig.spec.json',
-        useESM: true,
-      },
-    ],
-  },
-};
+});
